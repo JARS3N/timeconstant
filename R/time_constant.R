b420b20157a86512859701602288e4c0fee5d374
JARS3N <JARS3N@users.noreply.github.com>
2020-04-23 15:25:10 -0400

    Update time_constant.R

--- R/time_constant.R
+++ R/time_constant.R
@@ -1,6 +1,6 @@
 time_constant<-function(x){
   cumdif.counts<-diff(x$counts)
-  injection<-x$Time[which(cumdif.counts==min(cumdif.counts))-3]
+  injection<-x$Time[which(cumdif.counts==min(cumdif.counts))[1]-3]
   period<-injection + 30
   start<-x$counts[x$Time==x$Time[which(abs(x$Time-injection)==min(abs(x$Time-injection)))]]
   endP<-x$counts[x$Time==x$Time[which(abs(x$Time-period)==min(abs(x$Time-period)))]]
