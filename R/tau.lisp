6bdd986110528210e9c441cbc93513244970c85a
JARS3N <JARS3N@users.noreply.github.com>
2020-05-17 21:36:10 -0400

    Update tau.lisp

--- R/tau.lisp
+++ R/tau.lisp
@@ -1,5 +1,14 @@
 (defun tau (period delta-p delta-t) 
- "calculating time constant"
+ "calculating tau(time constant)"
  (abs(/ period (log (/ 1 (- 1 (/ delta-p delta-t)))))))
+
+(defun min-mean-5 (x)
+"find the min mean of 5 consecutive points"
+(setf n (- (length x) 1))
+(apply #'min
+(loop for i from 0 to (- n 4)
+      collect (mean (subseq x i (+ i 5)))
+ ))
+)
  
  
