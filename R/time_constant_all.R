80efe01d335f97543b43c930ca3bb303df141ce6
JARS3N <JARS3N@users.noreply.github.com>
2022-10-18 09:39:19 -0400

    Update time_constant_all.R

--- R/time_constant_all.R
+++ R/time_constant_all.R
@@ -1,7 +1,7 @@
 time_constant_all<-function(u){#where u is the directory of csv files to run
   require(dplyr)
   list.files(path=u,pattern="csv",full.names=TRUE) %>%
-    lapply(.,time_constant::pre_process) %>%
+    lapply(.,timeconstant::pre_process) %>%
     bind_rows(.) %>%
     group_by(.,Chan,fl) %>%
     summarize(TC=timeconstant::time_constant(data.frame(Time=Time,counts=counts)))
