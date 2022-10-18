ed325c7e21d55dbc1c1c534cb10f45c1227411a6
JARS3N <JARS3N@users.noreply.github.com>
2022-10-18 09:49:08 -0400

    Update proc.R

--- R/proc.R
+++ R/proc.R
@@ -5,5 +5,6 @@ endP<-COUNTS[TIME==period]
 endT<-timeconstant::lowest_avg_n_counts(COUNTS)
 deltaP <- start - endP
 deltaT <- start - endT
-abs(period/(log(1/(1 - (deltaP/deltaT)))))
+#abs(period/(log(1/(1 - (deltaP/deltaT)))))
+tau(period,deltaT,deltaP)
 }
